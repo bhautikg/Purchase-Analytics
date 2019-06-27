ed894252c907774061457402d39af4a216571e01
bhautikg <52224896+bhautikg@users.noreply.github.com>
2019-06-27 16:32:55 -0400

    Error Catching
    Added Error Catching
    catching non positive integers in the records

--- src/AnalyticsHelper.py
+++ src/AnalyticsHelper.py
@@ -1,36 +1,71 @@
+from decimal import Decimal
+
+def IsPosIntegerProd(record):
+    try:
+
+        if int(record[0])>=0 and int(record[3])>=0:
+            return True
+    except TypeError:
+        return False
+    except ValueError:
+        return False
+
+def IsPosIntegerOrd(record):
+    try:
+
+        if int(record[1])>=0 and int(record[3])>=0:
+            return True
+    except TypeError:
+        return False
+    except ValueError:
+        return False
+
 def ProdRecord(record, dict):
     """
     :param record: [product_id,product_name,aisle_id,department_id]
     :param dict: dictionary of entries
     :return: dict with record added or updated
     """
-    dict[record[0]] = record[3]
-    return True
+    if IsPosIntegerProd(record):
+        try:
+            dict[record[0]] = int(record[3])
+        except TypeError:
+            return False
+        return True
+    else:
+        raise ValueError('This record is invalid %s' % record)
 
-def DeptRecord(record, dept_id, dept_dict):
+def DeptRecord(record, prod_dict, dept_dict):
     """ 
     :param Order record: [order_id,product_id,add_to_cart_order,reordered]
     :param dict: dictionary of entries 
     :out put [department_id,number_of_orders,number_of_first_orders,percentage]
     :return: dict with record added or updated
     """
-    function_dict = {}
-    if dept_id in dept_dict:
-        function_dict = dept_dict[dept_id]
-        function_dict['number_of_orders'] = function_dict['number_of_orders'] + 1
+    if IsPosIntegerOrd(record):
+        dept_id = prod_dict[record[1]]
+        function_dict = {}
+        if dept_id in dept_dict:
+            function_dict = dept_dict[dept_id]
+            function_dict['number_of_orders'] = function_dict['number_of_orders'] + 1
 
-        #print (record[3])
-        #print(function_dict['number_of_orders'])
-        if record[3] == '0':
-            function_dict['number_of_first_orders'] = function_dict['number_of_first_orders'] + 1
-        function_dict['percentage'] = function_dict['number_of_first_orders']/ function_dict['number_of_orders']
-    else:
-        dept_dict[dept_id] = function_dict
-        function_dict['number_of_orders'] = 1
-        if record[3] =='0':
-            function_dict['number_of_first_orders'] = 1
+
+            if record[3] == '0':
+                function_dict['number_of_first_orders'] = function_dict['number_of_first_orders'] + 1
+            
+            ratio = function_dict['number_of_first_orders']/ function_dict['number_of_orders']
+            function_dict['percentage'] = round(ratio,2)
+        
         else:
-            function_dict['number_of_first_orders'] = 0
-        function_dict['percentage'] = function_dict['number_of_first_orders']/ function_dict['number_of_orders']
-
+            dept_dict[dept_id] = function_dict
+            function_dict['number_of_orders'] = 1
+            
+            if record[3] =='0':
+                function_dict['number_of_first_orders'] = 1
+            else:
+                function_dict['number_of_first_orders'] = 0
+            ratio = function_dict['number_of_first_orders']/ function_dict['number_of_orders']
+            function_dict['percentage'] = round(ratio,2)
+    else:
+        raise ValueError('This record is invalid %s' % record)
     return True
---
+++ src/PurchaseAnalyticsMain.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+import csv
+import AnalyticsHelper
+
+
+def main():
+    """
+
+    """
+    parser = argparse.ArgumentParser(description='Purchase Analytics')
+    parser.add_argument('OrdProdIn', metavar='orders file address', help='Input containing ordered products')
+    parser.add_argument('ProdIn', metavar='products file address', help='Input containing information about products')
+    parser.add_argument('output', metavar='output file addres', help='Output containing processed info deprtments and their statistics')
+    args = parser.parse_args()
+
+    
+    try:
+        with open(args.ProdIn, 'r') as ProdInFile:
+            infile = csv.reader(ProdInFile, delimiter=',')
+            next(infile)
+            prod_dict = {}
+            for i, record in enumerate(infile):
+                flag = AnalyticsHelper.ProdRecord(record, prod_dict)
+                if flag != True:
+                    break
+        
+
+        with open(args.OrdProdIn, 'r') as OrdProdInFile:
+            infile = csv.reader(OrdProdInFile, delimiter=',')
+            next(infile)
+            dept_dict = {}
+            for i, record in enumerate(infile):
+                AnalyticsHelper.DeptRecord(record, prod_dict, dept_dict)
+    except IOError:
+        print('One or both of the input files not found')
+        return
+    except ValueError as e:
+        print (str(e))
+        return
+
+    dept_prod_pair = zip(dept_dict.keys(), dept_dict.values())
+    dept_prod_pair = list(dept_prod_pair)
+    dept_prod_pair = sorted(dept_prod_pair, key=lambda row: (row[0]))
+    
+    with open(args.output, 'w', newline='\n') as analytics_outfile:
+        headers = ['department_id','number_of_orders','number_of_first_orders','percentage']
+        outfile = csv.DictWriter(analytics_outfile, fieldnames=headers)
+        outfile.writeheader()
+        for record in dept_prod_pair:
+            outfile.writerow(
+                {'department_id': record[0],
+                 'number_of_orders': record[1]['number_of_orders'],
+                 'number_of_first_orders': record[1]['number_of_first_orders'],
+                 'percentage': record[1]['percentage']}
+            )
+        #analytics_outfile.truncate(analytics_outfile.tell() - len(os.linesep))
+
+if __name__=="__main__":
+    main()
